4a81a93da40e42196cc838d76dde349730ccf36c
vberdv <vberdnikovv@gmail.com>
2019-06-19 19:50:42 +0300

    project created

---
+++ portfolio/web-vb/js/common.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({
+		type:"inline",
+		midClick: true
+	});
+
+	//*Новое видео
+	$(function(){
+      $(".player").mb_YTPlayer();
+    });	
+
+	$(".section_header").animated("fadeInUp", "fadeOutDown");
+
+	$(".animation_1").animated("flipInY", "fadeOutDown");
+	$(".animation_2").animated("fadeInLeft", "fadeOutDown");
+	$(".animation_3").animated("fadeInRight", "fadeOutDown");	
+
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+
+	$("input, select, textarea").jqBootstrapValidation();
+
+	$(".top_mnu ul a").mPageScroll2id();
+
+});
+
+// Загружает анимацию на главной странице
+$(window).load(function() {
+
+	$(".loader_inner").delay(600).fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
+	$(".top_text p").animated("fadeInUp", "fadeOutDown");
+
+}); 
